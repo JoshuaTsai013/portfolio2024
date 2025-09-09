be004cfac6e592bd9aac8b4cb688cf71fe52e932
JoshuaTsai013 <joshuatsai1213@gmail.com>
2025-09-09 17:36:19 +0800

    parallex position fixed

--- src/components/Parallax/index.jsx
+++ src/components/Parallax/index.jsx
@@ -3,12 +3,14 @@ import { motion, useScroll, useTransform } from "motion/react";
 
 function Parallex({ scrollY }) {
 
-  const { height } = useWindowDimensions();
-  const scaleY = useTransform(scrollY, [0, 1000], [1, 1.5]);
-  const moveY = useTransform(scrollY, [0, 1200], [0, 500]);
+  const { height, width } = useWindowDimensions();
+  const isMobile = width < 768;
+
+  const scaleY = useTransform(scrollY, [0, 1000], isMobile ? [1, 1.7] : [1, 1.5]);
+  const moveY = useTransform(scrollY, [0, 900], isMobile ? [0, 200] : [0, 600]);
   const moveSmallY = useTransform(scrollY, [0, 1000], [0, 50]);
-  const moveBottom = useTransform(scrollY, [0, 500], [0, 530]);
-  const scaleBottom = useTransform(scrollY, [200, 800], [1, 0.9]);
+  const moveBottom = useTransform(scrollY, [0, 500], isMobile ? [0, 230] : [0, 530]);
+  const scaleBottom = useTransform(scrollY, [200, 800], isMobile ? [1, 1.3] : [1, 0.6]);
 
   return (
     <div
@@ -18,14 +20,14 @@ function Parallex({ scrollY }) {
     >
       <motion.div className="h-screen w-full relative left-0 top-0">
         <motion.img
-          style={{ scaleY, transformOrigin: "top" }}
+          style={{ transformOrigin: "top" ,height: `${height + 400}px` }}
           className="h-screen absolute m-auto left-0 right-0 Top-0"
           src="ParallexImages/backgroundUpLineSize.png"
           alt="backgroundUpLineSize"
         />
         <motion.img
           style={{ y: moveY }}
-          className="h-1/3 absolute m-auto left-0 right-0 -top-4 bottom-0"
+          className="h-1/3 absolute m-auto left-0 right-0 bottom-24 "
           src="ParallexImages/sunSize.png"
           alt="sunSize"
         />
@@ -43,13 +45,13 @@ function Parallex({ scrollY }) {
         />
         <motion.img
           style={{ y: moveBottom, scaleY: scaleBottom, transformOrigin: "bottom" }}
-          className="h-1/2 absolute m-auto left-0 right-0 bottom-0"
+          className="h-1/2 absolute m-auto left-0 right-0 -bottom-2"
           src="ParallexImages/backgroundDownSize.png"
           alt="backgroundDownSize"
         />
         <motion.img
           style={{ y: moveBottom, scaleY: scaleBottom, transformOrigin: "bottom" }}
-          className="h-1/2 absolute m-auto left-0 right-0 bottom-0"
+          className="h-1/2 absolute m-auto left-0 right-0 -bottom-2"
           src="ParallexImages/gridLineSize.png"
           alt="gridLineSize"
         />
