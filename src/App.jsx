3891e9f3a77bb3428bab9d239a2ef261d7e5e70b
JoshuaTsai013 <joshuatsai1213@gmail.com>
2025-12-01 16:34:38 +0800

    feat: set up application routing, add a responsive header component, and add an opening video file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css'
 import { useMotionValue } from "motion/react";
 import SmoothScroll from "./components/SmoothScroll";
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/home';
+import Header from './components/Header';
 
 function App() {
   const scrollY = useMotionValue(0);
@@ -11,9 +13,18 @@ function App() {
   };
 
   return (
-    <SmoothScroll setScrollY={handleScrollY}>
-      <HomePage scrollY={scrollY} />
-    </SmoothScroll>
+    <>
+      <Header />
+      <SmoothScroll setScrollY={handleScrollY}>
+        <Routes>
+          <Route path="/" element={<HomePage scrollY={scrollY} />} />
+          <Route path="/AboutUs" element={<div>About Us Page</div>} />
+          <Route path="/Music" element={<div>Music Page</div>} />
+          <Route path="/Model" element={<div>3D Model Page</div>} />
+          <Route path="/Sign" element={<div>Sign Up/In Page</div>} />
+        </Routes>
+      </SmoothScroll>
+    </>
   )
 }
 
